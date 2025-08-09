ae443b140d51e4f711018cec588bb8acc2aea236
Bahrasemani <bahrasemanifatemeh@gmail.com>
2025-08-09 21:56:28 +0330

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1 style={{ fontSize: '2rem' }}>Evolve Club is finally online! 🎉</h1>
-    </main>
-  );
-}
