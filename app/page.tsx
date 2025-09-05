912f89aaadf478e01b7814855fee2918b293bcd3
Bahrasemani <bahrasemanifatemeh@gmail.com>
2025-09-05 10:31:58 +0330

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>به سایت ایوالو خوش آمدید 🚀</h1>
+      <p>این یک عکس تستی است:</p>
+      <img 
+        src="/new-image.jpg" 
+        alt="عکس جدید" 
+        style={{ maxWidth: "600px", marginTop: "20px" }} 
+      />
+    </main>
+  );
+}
